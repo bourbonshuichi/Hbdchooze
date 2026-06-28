e95260ce6cb06317a640057954c7d8e06f0dd429
bourbonshuichi <bourbonshuichi@gmail.com>
2026-06-28 16:23:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+function openLetter(){cover.style.display="none";letter.classList.remove("hidden");for(let i=0;i<40;i++){let h=document.createElement("div");h.className="heart";h.innerHTML="🤍";h.style.left=Math.random()*100+"vw";h.style.animationDelay=Math.random()*3+"s";document.body.appendChild(h);}}music.onclick=()=>alert("ใส่ลิงก์เพลงโปรดของคุณในภายหลัง เช่น Turning Page");
